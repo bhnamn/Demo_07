fa1ccf7feef501621e2f87873e3ca045bef7023b
bhnamn <borhan.amini@gmail.com>
2016-02-02 12:52:28 +0200

    a bit more developed ver way map is ready

--- Teh2_DifNumDifFile/Program.cs
+++ Teh2_DifNumDifFile/Program.cs
@@ -14,20 +14,29 @@ namespace Teh2_DifNumDifFile
             List<int> list = new List<int>();
             // boolean variable, just check for looping
             bool result;
+            string line;
             // ask numbers
             do
             {
                 // ask number from the user (read one line)
                 Console.Write("Give a number > ");
-                string line = Console.ReadLine();
+                line = Console.ReadLine();
                 // try to read number from the given line
                 int number;
                 result = int.TryParse(line, out number);
                 // number (integer) was given correctly, use it..
-                if (result==true) Console.WriteLine("true");
-                else Console.WriteLine("false");
+                if (result)
+                {
+                    // Write enterd line to the integer file
+                }
+                else
+                {
+                    // Write enterd line to the decimal file
+                }
 
-            } while (result);
+
+
+            } while (line.Length != 0);
         }
     }
 }
